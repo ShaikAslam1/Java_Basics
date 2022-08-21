5f1d9d43a12c5484717059ae6a9faa8acaa02c45
asshaik <aslam.shaik.ext@nokia.com>
2022-08-21 11:44:11 +0530

    patternSyntaxChecker and javaStringToken hackerrank

--- src/com/hackerrank/JavaStringToken.java
+++ src/com/hackerrank/JavaStringToken.java
@@ -8,6 +8,18 @@ public class JavaStringToken {
         Scanner scan = new Scanner(System.in);
         String s = scan.nextLine();
         // Write your code here.
+        if (s.trim().equals("")) {
+            System.out.println("0");
+        } else if (!s.contains("!") && !s.contains(",") && !s.contains("?") && !s.contains(".")
+                && !s.contains("_") && !s.contains("'") && !s.contains("@") && !s.contains(" ")) {
+            System.out.println("0");
+        } else {
+            String[] stringSplit = s.trim().split("[!,?._'@\\s]+");
+            System.out.println(stringSplit.length);
+            for (String str : stringSplit) {
+                System.out.println(str);
+            }
+        }
         scan.close();
     }
 }
