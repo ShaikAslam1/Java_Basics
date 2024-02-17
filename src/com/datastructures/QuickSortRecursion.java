3deca29e7eca812884bcf573b1f5d76c1fb71d36
asshaik <aslam.shaik.ext@nokia.com>
2024-02-17 22:55:19 +0530

    fix quick sort - recursion

--- src/com/datastructures/QuickSortRecursion.java
+++ src/com/datastructures/QuickSortRecursion.java
@@ -8,48 +8,46 @@ public class QuickSortRecursion {
         if (startIndex >= endIndex) {
             return;
         }
+        int pivotIndex = partition(input, startIndex, endIndex);
 
-        int pivotPosition = partition(input, startIndex, endIndex);
-
-        quickSort(input, startIndex, pivotPosition-1);
-        quickSort(input, pivotPosition+1, endIndex);
+        quickSort(input, startIndex, pivotIndex - 1);
+        quickSort(input, pivotIndex + 1, endIndex);
     }
 
-    private static int partition(int[] input, int startIndex, int endIndex) {
-        int pivot = input[startIndex];
-        // count number of elements < p
-        int count = 0;
-        for (int i=startIndex+1; i<endIndex; i++) {
-            if (input[i] < pivot) {
-                count++;
+    public static int partition(int[] input, int startIndex, int endIndex) {
+        int pivotElement = input[startIndex];
+        int smallerNumCount = 0;
+        for (int i = startIndex + 1; i <= endIndex; i++) {
+            if (input[i] < pivotElement) {
+                smallerNumCount++;
             }
         }
-        int pivotPos = startIndex + count;
-        int temp = input[startIndex];
-        input[startIndex] = input[pivotPos];
-        input[pivotPos] = temp;
-
+        int temp = input[startIndex + smallerNumCount];
+        input[startIndex + smallerNumCount] = pivotElement;
+        input[startIndex] = temp;
         int i = startIndex;
         int j = endIndex;
-
-        while (i<pivotPos && j>pivotPos) {
-            if (input[i]<=input[pivotPos]) {
+        while (i < j) {
+            if (input[i] < pivotElement) {
                 i++;
-            } else if (input[j]>input[pivotPos]) {
+            } else if (input[j] >= pivotElement) {
                 j--;
             } else {
-                int temp_ = input[i];
+                temp = input[i];
                 input[i] = input[j];
-                input[j] = temp_;
-                i++;j--;
+                input[j] = temp;
+                i++;
+                j--;
             }
         }
-        return pivotPos;
+        return startIndex + smallerNumCount;
     }
 
     public static void main(String[] args) {
-        int[] input = {4, 3, 8, 4, 6, 5};
-        quickSort(input, 0, input.length-1);
-        Arrays.stream(input).forEach(System.out::print);
+        int[] input = {552, 95, 337, 413, 899, 784, 962, 432, 275, 586, 810, 685, 267, 814, 13, 190, 67, 201, 164,
+                657, 462, 313, 310, 366, 848, 375, 751, 751};
+//        int[] input = {4, 3, 8, 4, 6, 5};
+        quickSort(input, 0, input.length - 1);
+        Arrays.stream(input).forEach(e -> System.out.print(e + " "));
     }
 }
