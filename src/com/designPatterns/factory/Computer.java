7084f66a7ce6b48579bcdf3a1f1fed702c90354f
asshaik <aslam.shaik.ext@nokia.com>
2024-03-30 12:58:09 +0530

    factory design pattern

---
+++ src/com/designPatterns/factory/Computer.java
@@ -0,0 +1,17 @@
+package com.designPatterns.factory;
+
+public abstract class Computer {
+    /*
+    The factory design pattern is used when we have a superclass with multiple
+    subclasses and based on input, we need to return one of the subclass
+     */
+
+    public abstract String getRAM();
+    public abstract String getHDD();
+    public abstract String getCPU();
+
+    @Override
+    public String toString() {
+        return "RAM= " + this.getRAM() + ", HDD= " + this.getHDD() + ", CPU= " + this.getCPU();
+    }
+}
